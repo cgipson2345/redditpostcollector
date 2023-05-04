84545795b3596211675191cbf05460a2294c676f
rdunc012 <97066683+rdunc012@users.noreply.github.com>
2023-05-04 14:19:05 -0700

    No comments yet

--- crawler.py
+++ crawler.py
@@ -27,48 +27,55 @@ for file_name in os.listdir("Data"):
 # Posts list to hold each post's data
 posts = list()
 
+# Create set to store post ids
+seen_ids = set()
+
 # Go through each subreddit
-print("\nStart crawling subreddits...\n")
+print("\nStart crawling subreddits...")
 for subreddit_name in subreddits:
-    print("Started crawling subreddit:",subreddit_name)
+    print("\tStarted crawling subreddit:",subreddit_name)
     # Currently grabs the 10 hottest posts, can change to what we want later
     cur_subreddit = reddit.subreddit(subreddit_name).hot(limit=10)
     # Loop over each post in the current subreddit
     for post in cur_subreddit:
-        # Store the data in dictionary
-        post_data = {
-            'subreddit': subreddit_name,    # Subreddit's name
-            'body': post.selftext,          # Body
-            'title': post.title,            # Title
-            'id': post.id,                  # Id
-            'score': post.score,            # Upvotes
-            'url': post.url,                # Image in post
-            'permalink': post.permalink,    # URL of post
-        }
+        # Check if post id is in the seen_ids set
+        if post.id not in seen_ids:
+            # Add post id to seen_ids set
+            seen_ids.add(post.id)
+            # Store the data in dictionary
+            post_data = {
+                'subreddit': subreddit_name,    # Subreddit's name
+                'body': post.selftext,          # Body
+                'title': post.title,            # Title
+                'id': post.id,                  # Id
+                'score': post.score,            # Upvotes
+                'url': post.url,                # Image in post
+                'permalink': post.permalink,    # URL of post
+            }
         # If a post contains a URL to an html page, get title of that page, and add title as an additional field of the post, 
         # that is, include it in the JSON of the post, so it becomes searchable in Part B.
         if post.url.endswith('.html'):
             # Try to get the URL in the post
             try:
-                # Send request to the post URL
-                response = requests.get(post.url)
+                # Send request to the post URL, skips if 10 seconds pass
+                response = requests.get(post.url, timeout=10)
                 # Parse using BeautifulSoup
                 soup = BeautifulSoup(response.content, 'html.parser')
                 # Add the URL's title to the post's data
                 post_data['page_title'] = soup.title.string
             # Failed to get the URL
             except Exception as e:
-                print('ERROR: Failed to retrieve page title for {}: {}'.format(post.url, e))
+                print('\tERROR: Failed to retrieve page title for {}: {}'.format(post.url, e))
         # Append the current data to the current post
         posts.append(post_data)
-    print("Finished crawling subreddit:",subreddit_name)
-print("\nFinished crawling subreddits...")
+    print("\tFinished crawling subreddit:",subreddit_name)
+print("Finished crawling subreddits...")
 
 # Current file that is written too
 file_number = 0
 #currently set file to 1 mb and raw data to 10 mb for testing
 min_file_size = 1 * MB  # File size
-min_data_size = 10 * MB  # Raw data size
+min_data_size = 5 * MB  # Raw data size
 #this tests current file size
 data = []
 running_total = 0
@@ -80,20 +87,20 @@ while running_total <= min_data_size:
     for post_data in posts:
         if len(json.dumps(data)) >= min_file_size:
             filename = f"Data/fileNum{file_number}.json"
+            file_number += 1
             with open(filename,"w") as f:
                 for d in data:
                     json.dump(d, f)
                     f.write('\n')
+                f.flush()    
                 file_size_mb = os.path.getsize(filename) / MB
-                print(f"File {filename} size: {file_size_mb:.2f} MB")
-            # Increase the file number and clear the data list
-            file_number += 1
+                print(f"\t{filename} size: {file_size_mb:.2f} MB")
+            # Clear the data list
             data = []
         # Append the current data to the data list
         data.append(post_data)
         #print(json.dumps(data))
         running_total += len(json.dumps(post_data))
-
         # Write the remaining data into the last file
         if data and sum(os.path.getsize(f"Data/{f}") for f in os.listdir("Data")) >= min_data_size:
             filename = f"Data/fileNum{file_number}.json"
