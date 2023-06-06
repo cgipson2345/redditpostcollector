f09e07a17d9a747179f8f6ec9bc153fe90dcb80c
rdunc012 <97066683+rdunc012@users.noreply.github.com>
2023-06-05 18:16:13 -0700

    Basic indexing

--- pylucene.py
+++ pylucene.py
@@ -3,6 +3,7 @@ logging.disable(sys.maxsize)
 
 import lucene
 import os
+import json
 from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
@@ -12,26 +13,10 @@ from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, I
 from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
 from org.apache.lucene.search.similarities import BM25Similarity
 
-sample_doc = [
-    {
-        'title' : 'A',
-        'context' : 'lucene is a useful tool for searching and information retrieval'
-        },
-    {
-        'title' : 'B',
-        'context' : 'Bert is a deep learning transformer model for encoding textual data'
-    },
-    {
-        'title' : 'C',
-        'context' : 'Django is a python web framework for building backend web APIs'
-    }
-]
-
-
-def create_index(dir):
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    store = SimpleFSDirectory(Paths.get(dir))
+def create_index(post_data, index_dir):
+    if not os.path.exists(index_dir):
+        os.mkdir(index_dir)
+    store = SimpleFSDirectory(Paths.get(index_dir))
     analyzer = StandardAnalyzer()
     config = IndexWriterConfig(analyzer)
     config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
@@ -46,37 +31,79 @@ def create_index(dir):
     contextType.setTokenized(True)
     contextType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
 
-    for sample in sample_doc:
-        title = sample['title']
-        context = sample['context']
+    # Add post data
+    for post in post_data:
+        subreddit = post['subreddit']
+        body = post['body']
+        title = post['title']
+        id = post['id']
+        score = post['score']
+        url = post['url']
+        permalink = post['permalink']
+        author = post['author']
+
+        #print(f"Getting post: {id}")
 
         doc = Document()
-        doc.add(Field('Title', str(title), metaType))
-        doc.add(Field('Context', str(context), contextType))
+        doc.add(Field('Subreddit', str(subreddit), contextType))
+        doc.add(Field('Body', str(body), contextType))
+        doc.add(Field('Title', str(title), contextType))
+        doc.add(Field('Id', str(id), metaType))
+        doc.add(Field('Score', str(score), metaType))
+        doc.add(Field('URL', str(url), metaType))
+        doc.add(Field('Permalink', str(permalink), metaType))
+        doc.add(Field('Author', str(author), metaType))
+        
+        # Add comment data
+        for comment in post.get('comments', []):
+            comment_id = comment['id']
+            comment_body = comment['body']
+            comment_score = comment['score']
+            comment_author = comment['author']
+            
+            doc.add(Field('Comment_Id', str(comment_id), metaType))
+            doc.add(Field('Comment_Body', str(comment_body), contextType))
+            doc.add(Field('Comment_Score', str(comment_score), metaType))
+            doc.add(Field('Comment_Author', str(comment_author), metaType))
         writer.addDocument(doc)
+
     writer.close()
 
-def retrieve(storedir, query):
+def retrieve(storedir, query, num_docs):
     searchDir = NIOFSDirectory(Paths.get(storedir))
     searcher = IndexSearcher(DirectoryReader.open(searchDir))
     
-    parser = QueryParser('Context', StandardAnalyzer())
+    parser = QueryParser('Body', StandardAnalyzer())
     parsed_query = parser.parse(query)
 
-    topDocs = searcher.search(parsed_query, 10).scoreDocs
+    topDocs = searcher.search(parsed_query, num_docs).scoreDocs
     topkdocs = []
     for hit in topDocs:
         doc = searcher.doc(hit.doc)
         topkdocs.append({
             "score": hit.score,
-            "text": doc.get("Context")
+            "text": doc.get("Body")
         })
     
     print(topkdocs)
 
+def load_data(data_dir):
+    post_data = []
+    for file_name in os.listdir(data_dir):
+        if file_name.endswith('.json'):
+            with open(os.path.join(data_dir, file_name), 'r') as f:
+                post_data.extend(json.load(f))
+    return post_data
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
-create_index('sample_lucene_index/')
-retrieve('sample_lucene_index/', 'web data')
-
-
+num_docs = 10 # Top docs from query
+data_dir = 'Data/'
+index_dir = 'sample_lucene_index/'
+query = 'best game engine'
+    
+print(f"Loading posts from {data_dir} folder")
+post_data = load_data(data_dir)
+print(f"Creating index from post data")
+create_index(post_data, index_dir)
+print(f"Top {num_docs} scores for query: {query}")
+retrieve(index_dir, query, num_docs)
