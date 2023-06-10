cefdb6bbfd4db2247e27842f2d57937a5c7c36c7
Shuqiao Yu - syu110 <73509954+xlotionx@users.noreply.github.com>
2023-06-10 01:32:09 -0700

    Update main.py

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request
+import lucene
+from search import retrieve  # assuming we have a search.py file that contains the PyLucene functions
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
 def index():
+    results = []
     if request.method == 'POST':
-        keyword = request.form['keyword']
-        # perform search logic here
-        # replace the following with the appropriate search logic
-        results = ['Result 1', 'Result 2', 'Result 3']
-        return render_template('index.html', keyword=keyword, results=results)
-    
-    return render_template('index.html')
-
+        query = request.form['query']
+        # assuming 'index_dir' is a directory containing the PyLucene index
+        # and 'num_docs' is the number of documents you want to retrieve
+        results = retrieve('index_dir', query, num_docs=10)
+    return render_template('index.html', results=results)
 
 if __name__ == '__main__':
+    lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     app.run(debug=True)
